a40dea7910eef380c2d71fef1cbe74e6b76650f1
mohammad sookhtesaraei <114998362+mohammadsookhtesaraei@users.noreply.github.com>
2024-01-02 15:17:48 +0330

    palindrome2

--- Part1/3-palindrome/main.js
+++ Part1/3-palindrome/main.js
@@ -15,3 +15,13 @@ console.log(palindrome("دود"));
 console.log(palindrome("hello world"));
 console.log(palindrome("abba"));
 
+
+const secondWay=(str)=>{
+    const result=str.split("").every((char,index)=>{
+        return char === str[str.length - index - 1]
+    })
+
+    return result;
+}
+
+console.log(secondWay("گرگ"));
