4488b86bc19794fcea739d5326d717225dc2f1c5
mohammad sookhtesaraei <114998362+mohammadsookhtesaraei@users.noreply.github.com>
2024-01-02 15:00:27 +0330

    stringReverse

---
+++ Part1/1-stringReverse/main.js
@@ -0,0 +1,12 @@
+
+
+// Mohammad Soukhtehsaraei
+
+const StringReverse=(str) => {
+    const result=str.split("").reverse().join(" ");
+    return result;
+};
+
+
+console.log(StringReverse("Mohammad Soukhtehsaraei"));
+console.log(StringReverse("hello world"));
